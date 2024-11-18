7b0b470c90fb7668a37160635eaf8fe05bebca8b
KirillChel39 <alexchel39@gmail.com>
2024-11-18 20:15:05 +0300

    Add files via upload

---
+++ lesen1.js
@@ -0,0 +1,5 @@
+console.log("Hello world!");
+var a = "hello word";
+const a =  true;
+const b = false;
+
---
+++ less2.js
@@ -0,0 +1,26 @@
+console.log("1");
+const Dz = "Типа дз";
+const name = "kirill";
+const age = 14;
+const hobby = "basketball";
+const favoriteNumber = 77;
+const New = 2024;
+const Old = 2010;
+const sum = New - Old;
+console.log ("Да, мне " + sum + " года.");
+const yearofbirth = 2010
+let currentyear = 2024
+let myage = currentyear - yearofbirth
+
+console.log('#3 - ' +  'Привет, мне ' + myage + ' года.')
+let isGraduated
+let explicitundefined
+let implicitundefined = undefined
+const fornull = null
+
+console.log("#2 - " +  explicitundefined + '||' + implicitundefined + '||' + fornull);
+
+
+
+
+
